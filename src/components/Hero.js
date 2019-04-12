cddf9e175e7c755afda8ee35c4d4de1d1815b6bf
Yeimer Molina <yeimer.molina@gmail.com>
2019-04-11 20:22:24 -0500

    Add Vertical nav component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,9 @@
 import React from "react"
+import VerticalNav from "./VerticalNav"
 
 const Hero = () => (
   <div className="Hero">
+    <VerticalNav />
     <h1 className="Hero__title">
       YEIMER <br /> MOLINA
     </h1>
---
+++ src/components/VerticalNav.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const VerticalNav = () => (
+  <nav className="VerticalNav">
+    <a href="/" className="VerticalNav__link">
+      CONTACT
+    </a>
+    <a href="/" className="VerticalNav__link">
+      PROJECTS
+    </a>
+    <a href="/" className="VerticalNav__link">
+      ABOUT
+    </a>
+    <div className="VerticalNav__line" />
+  </nav>
+)
+
+export default VerticalNav
