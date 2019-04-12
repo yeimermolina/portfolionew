28fc0eee91c797cba039b8122265188721d44354
Yeimer Molina <yeimer.molina@gmail.com>
2019-04-11 19:59:55 -0500

    Add Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Hero = () => (
+  <div className="Hero">
+    <h1 className="Hero__title">
+      YEIMER <br /> MOLINA
+    </h1>
+  </div>
+)
+
+export default Hero
